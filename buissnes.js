5ef329c3ea1f1b3134203f5bb195e3180aa904d3
StaroVern <151537182+StaroVern@users.noreply.github.com>
2024-03-12 13:03:34 -0400

    1buizness
    qqw

---
+++ buissnes.js
@@ -0,0 +1,12 @@
+const finance = require('javascript-finance');
+
+function calculateNpv(cashFlows, interestRate) {
+    const presentValue = cashFlows.map((cf, i) => cf / (1 + interestRate) ** i);
+    return finance.sum(presentValue);
+}
+
+// Пример использования:
+const cashFlows = [100, 200, -300, 400];
+const interestRate = 0.1;
+const npv = calculateNpv(cashFlows, interestRate);
+console.log(`NPV: ${npv}`);
